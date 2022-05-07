79c4ba5a5d6ddc4c81ad4549bdfcdc9a624ae810
swatibersurda <swatibersurda@gmail.com>
2022-05-07 10:57:17 +0530

    addingloginbackend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,7 +65,7 @@ const router = express.Router();
         }
    })
 })
-router.post("/login",(req,res)=>{
+router.get("/login",(req,res)=>{
   const {mobileno} = req.body
   User.findOne({mobileno:mobileno},(err,user)=>{
       if(user){
